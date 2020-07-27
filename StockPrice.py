d363ed09711053034b6607304e7a1ec7a8fdb441
Peter Gemmell <petergemmell65@hotmail.com>
2020-07-27 14:55:00 +0100

    line graph returning closing price history up to todays date

--- StockPrice.py
+++ StockPrice.py
@@ -15,6 +15,18 @@ plt.style.use('fivethirtyeight')
 
 
 # Here we are going to get the stock quote. Creating a variable called df, short for data frame.
-df = web.DataReader('AAPL', data_source = 'yahoo', start = '2015-01-01', end = '2020-02-17')
+df = web.DataReader('AAPL', data_source = 'yahoo', start = '2015-01-01', end = '2020-07-27')
+
 # Show the data.
-print(df)
+# print(df) Will print the historic stock data in the terminal.
+
+# Get the number of rows and columns in the data set.
+print(df.shape)
+
+# Visualise the closing price history. Running plt.show() opens a line graph showing closing price history over the years.
+plt.figure(figsize=(16,8))
+plt.title('Close Price History')
+plt.plot(df['Close'])
+plt.xlabel('Date', fontsize=18)
+plt.ylabel('Close Price GBP(£)', fontsize=18)
+plt.show()
