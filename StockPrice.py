4ef074a120603bdcd9a0f84468112015fa1eeb6d
Peter Gemmell <petergemmell65@hotmail.com>
2020-07-27 17:34:44 +0100

    reached issue of converting data from 2d to 3d

--- StockPrice.py
+++ StockPrice.py
@@ -1,3 +1,4 @@
+
 # This program uses an artificial recurrent nueral network called Long Short Term Memory or (LSTM) for short.
 # We are going to use the LSTM to predict the closing price of a particular company's stock, using the past 60 day stock price.
 
@@ -50,3 +51,30 @@ scaled_data = scaler.fit_transform(dataset)
 
 # Create the training dataset
 # Create the scaled training dataset
+# This will contain all the values from index 0 to training_data_len and also all of the columns.
+train_data = scaled_data[0:training_data_len, :]
+
+# Split the data into x_train and y_train datasets
+# x_train will be the independent training features, y_train will be the dependent features.
+x_train = []
+y_train = []
+
+# Here we are appending the last 60 values to the x_train dataset. This does not reach i. We also get the 0 column.
+# On the loop x_train will contain 60 values and those values will be indexed from 0 to 59 and the y_train will contain the
+# 61st value at postion 60.
+
+for i in range(60, len(train_data)):
+    x_train.append(train_data[i-60:i,0])
+    y_train.append(train_data[i, 0])
+    if i<= 60:
+        # print(x_train)
+        # print(y_train) prints the 61st value that we want our model to predict.
+        # print()
+
+        # Convert the x_train and y_train to numpy arrays. DIDNT RUN THIS.
+        x_train, y_train = np.array(x_train), np.array(y_train)
+
+# Reshape the data. This is becuase an LSTM network expects the data to be 3 dimensional and right now the data set is
+# 2 dimensional.
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+print(x_train.shape)
